0019244a5db8b3e54051425cd766d13f416ffba6
Neuenroth Christian <christian.neuenroth@contitech.de>
2025-06-25 11:43:39 +0000

    prepare BomTest

---
+++ src/test/scala/ct/dna/lakehouse/catalog/dw_md/showcase/BomTest.scala
@@ -0,0 +1,21 @@
+package ct.dna.lakehouse.catalog.dw_md.showcase
+
+import ct.dna.lakehouse.TestSuiteWithEnvironment
+
+class BomTest extends TestSuiteWithEnvironment /* some other suitable as well?*/ {
+
+  "dw_md.showcase.Bom" should "be empty when 'sr.showcase.Bom' table is empty" in {
+
+//implicit class that extends Table
+// =>  def mkEmpty: Dataframe
+// => def update(excelspreadsheet): Dataframe //update/override?
+// => def snapshot : Dataframe
+// => def executeTransformation // (implicit testSuite:TestSuiteWithEnvironment)?
+//
+
+    // sr.showcase.Bom.mkEmpty
+    // dw_md.showcase.Bom.update(excel)
+    // assert(dw_md.showcase.Bom.snapshot.isEmpty)
+
+  }
+}
