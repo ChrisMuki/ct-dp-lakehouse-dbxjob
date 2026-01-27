e94188b6a7b4278180dcde7329153c7523b8a3cd
Christian Neuenroth <uia00402@contiwan.com>
2026-01-27 12:36:01 +0100

    fix: adapt postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,61 +5,13 @@ set -euo pipefail
 ##### Clean up #####
 ####################
 echo "Cleaning up old build artifacts..."
+rm -rf .bloop
 rm -rf .metals
 rm -rf .vscode
 rm -rf project/project/
 rm -rf project/target/
 rm -rf project/metals.sbt
 rm -rf target/
-
-####################
-##### JVM OPTS #####
-####################
-JVMOPTS=".jvmopts"
-TEMPLATE=".jvmopts.template"
-
-# Immer neu starten: Template kopieren oder leere Datei anlegen
-if [ -f "$TEMPLATE" ]; then
-    cp "$TEMPLATE" "$JVMOPTS"
-else
-  : > "$JVMOPTS"   # create empty file
-fi
-
-
-escape_jvm_value() {
-    local raw="$1"
-    raw="${raw//\\/\\\\}"   # escape existing backslashes
-    raw="${raw//|/\\|}"        # escape pipes so JVM reads them literally
-    printf '%s' "$raw"
-}
-
-append_jvm_opt() {
-    local key="$1"
-    local value
-    value="$(escape_jvm_value "$2")"
-    printf -- '-D%s=%s\n' "$key" "$value" >> "$JVMOPTS"
-}
-
-if [ -z "${CONTAINER_NEEDED_PROXY:-}" ]; then
-    :
-else
-    append_jvm_opt http.proxyHost "$JAVA_proxyHost"
-    append_jvm_opt http.proxyPort "$JAVA_proxyPort"
-    append_jvm_opt http.nonProxyHosts "$JAVA_nonProxyHosts"
-    append_jvm_opt https.proxyHost "$JAVA_proxyHost"
-    append_jvm_opt https.proxyPort "$JAVA_proxyPort"
-    append_jvm_opt https.nonProxyHosts "$JAVA_nonProxyHosts"
-    append_jvm_opt java.net.useSystemProxies "true"
-
-    # JVM-Optionen auch Coursier explizit mitgeben (Metals nutzt eigenen Coursier)
-    if [ -f "$JVMOPTS" ]; then
-      # aus Zeilen eine einzelne Zeile mit Leerzeichen machen
-      COURSIER_JVM_OPTS="$(tr '\n' ' ' < "$JVMOPTS")"
-      export COURSIER_JVM_OPTS
-    fi
-fi
-
-  echo "$JVMOPTS created/patched"
 
 
 ####################
